b487ee0ea5696858865761c03998a465ff7d9d84
Daniel Mihai <danikmihai23@mail.ru>
2024-10-14 15:48:25 +0300

    Initial commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow">
+    <meta name="description" content="Welcome to Daniel Mihai's portfolio! I am a Fullstack developer with experience in Laravel, PHP, JavaScript and many other technologies. Check out my projects and see how I can help you.">
+    <meta name="keywords" content="Fullstack разработчик, веб-разработка, Laravel, PHP, JavaScript, портфолио, сайты, приложения, Fullstack developer, web development, Laravel, PHP, JavaScript, portfolio, websites, applications">
+    <meta property="og:title" content="Daniel Mihai - Software developer">
+    <meta property="og:description" content="Check out my work and find out how I can help your business.">
+    <meta property="og:image" content="URL_к_изображению_вашего_портфолио">
+    <meta property="og:url" content="URL_вашего_портфолио">
+    <meta property="og:type" content="website">
+    <!-- Подключение CSS -->
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+    <title>Daniel Mihai - Software developer</title>
+</head>
+<body>
+
+</body>
+</html>
