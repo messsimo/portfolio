e2cb77b169124c8bf4ab52f0df58c6446e94cd44
Daniel Mihai <danikmihai23@mail.ru>
2024-10-16 13:56:18 +0300

    Skills block

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -63,5 +63,53 @@
             <img src="{{ asset('images/hero.png') }}">
         </div>
     </div>
+
+    <!-- Блок Скиллов -->
+    <div class="skills">
+        <h2>My <b>Skills</b></h2>
+
+        <div class="skills-container">
+            <div class="block">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50"><path d="M9,7l3,34l14,4l14-4c1-11.33,2-22.67,3-34H9z M33.76,35l-7.77,2l-7.76-2l-0.39-5h3.86l0.18,2L26,32.62L30.17,32l0.41-5	H17.59l0.96-12H34l0.7,6H31l-0.23-2h-8.36l-0.32,4h12.66L33.76,35z"></path></svg>
+                <span>HTML</span>
+            </div>
+            <div class="block active">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50"><path fill-rule="evenodd" d="M 42 6 L 39 40 L 25 44 L 11 40 L 8 6 Z M 16.800781 28 L 20.800781 28 L 20.898438 30.5 L 25 31.898438 L 29.101563 30.5 L 29.398438 26 L 20.601563 26 L 20.398438 22 L 29.601563 22 L 29.898438 18 L 16.101563 18 L 15.800781 14 L 34.101563 14 L 33.601563 22 L 32.898438 33.5 L 25 36.101563 L 17.101563 33.5 Z"></path></svg>
+                <span>CSS</span>
+            </div>
+            <div class="block">
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50"><path d="M 43.335938 4 L 6.667969 4 C 5.195313 4 4 5.195313 4 6.667969 L 4 43.332031 C 4 44.804688 5.195313 46 6.667969 46 L 43.332031 46 C 44.804688 46 46 44.804688 46 43.335938 L 46 6.667969 C 46 5.195313 44.804688 4 43.335938 4 Z M 27 36.183594 C 27 40.179688 24.65625 42 21.234375 42 C 18.140625 42 15.910156 39.925781 15 38 L 18.144531 36.097656 C 18.75 37.171875 19.671875 38 21 38 C 22.269531 38 23 37.503906 23 35.574219 L 23 23 L 27 23 Z M 35.675781 42 C 32.132813 42 30.121094 40.214844 29 38 L 32 36 C 32.816406 37.335938 33.707031 38.613281 35.589844 38.613281 C 37.171875 38.613281 38 37.824219 38 36.730469 C 38 35.425781 37.140625 34.960938 35.402344 34.199219 L 34.449219 33.789063 C 31.695313 32.617188 29.863281 31.148438 29.863281 28.039063 C 29.863281 25.179688 32.046875 23 35.453125 23 C 37.878906 23 39.621094 23.84375 40.878906 26.054688 L 37.910156 27.964844 C 37.253906 26.789063 36.550781 26.328125 35.453125 26.328125 C 34.335938 26.328125 33.628906 27.039063 33.628906 27.964844 C 33.628906 29.109375 34.335938 29.570313 35.972656 30.28125 L 36.925781 30.691406 C 40.171875 32.078125 42 33.496094 42 36.683594 C 42 40.117188 39.300781 42 35.675781 42 Z"></path></svg>
+                <span>JavaScript</span>
+            </div>
+            <div class="block">
+               <img src="{{ asset('images/php.png') }}" alt="PHP">
+               <span>PHP</span>
+            </div>
+            <div class="block">
+                <img src="{{ asset('images/laravel.png') }}" alt="Laravel">
+                <span>Laravel</span>
+            </div>
+            <div class="block">
+                <img src="{{ asset('images/sql.png') }}" alt="SQL">
+                <span>SQL</span>
+            </div>
+            <div class="block">
+                <img src="{{ asset('images/mysql.png') }}" alt="mySQL">
+                <span>mySQL</span>
+            </div>
+            <div class="block">
+                <img src="{{ asset('images/docker.png') }}" alt="Docker">
+                <span>Docker</span>
+            </div>
+            <div class="block">
+                <img src="{{ asset('images/git.png') }}" alt="Git">
+                <span>Git</span>
+            </div>
+            <div class="block">
+                <img src="{{ asset('images/figma.png') }}" alt="Figma">
+                <span>Figma</span>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
