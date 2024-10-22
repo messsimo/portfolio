c5b664fe858bc95d0729f9af030cc7cd6ac6ebe2
Daniel Mihai <danikmihai23@mail.ru>
2024-10-22 11:26:55 +0300

    adaptation: exp. block

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -226,7 +226,7 @@
         <div class="exp-container">
             <div class="block">
                 <div class="block--top">
-                    <h3>Middle Web Developer at Oxem Studio</h3>
+                    <h3>Junior Web Developer at Oxem Studio</h3>
                     <p>August 2024 – Present</p>
                 </div>
                 <span>Developed custom web applications and websites for clients in e-commerce, fintech, and education, leading full-cycle project development using Laravel, MySQL, and RESTful APIs. Optimized processes to reduce project delivery times by 15% and collaborated with clients to define technical specifications while ensuring clear communication throughout development. Conducted code reviews to maintain quality and built interactive user interfaces.</span>
